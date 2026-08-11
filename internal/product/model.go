c5bedca158b8c6350911610c751a031fe9868afb
Ebuka <chukwuebukanwokike2007@gmail.com>
2026-08-11 11:14:16 +0100

    update: added catrgory id to product model structs

--- internal/product/model.go
+++ internal/product/model.go
@@ -4,6 +4,7 @@ import "time"
 
 type Product struct {
 	ID          string    `json:"id"`
+	CategoryID  *string   `json:"category_id,omitempty"`
 	Name        string    `json:"name"`
 	Slug        string    `json:"slug"`
 	Description string    `json:"description"`
@@ -17,25 +18,27 @@ type Product struct {
 }
 
 type CreateProductRequest struct {
-	Name        string `json:"name" binding:"required"`
-	Slug        string `json:"slug" binding:"required"`
-	Description string `json:"description"`
-	SKU         string `json:"sku" binding:"required"`
-	Price       int64  `json:"price" binding:"gte=0"`
-	Currency    string `json:"currency" binding:"required,len=3"`
-	ImageURL    string `json:"image_url"`
-	Active      *bool  `json:"active"`
+	CategoryID  *string `json:"category_id"`
+	Name        string  `json:"name" binding:"required"`
+	Slug        string  `json:"slug" binding:"required"`
+	Description string  `json:"description"`
+	SKU         string  `json:"sku" binding:"required"`
+	Price       int64   `json:"price" binding:"gte=0"`
+	Currency    string  `json:"currency" binding:"required,len=3"`
+	ImageURL    string  `json:"image_url"`
+	Active      *bool   `json:"active"`
 }
 
 type UpdateProductRequest struct {
-	Name        string `json:"name"`
-	Slug        string `json:"slug"`
-	Description string `json:"description"`
-	SKU         string `json:"sku"`
-	Price       *int64 `json:"price"`
-	Currency    string `json:"currency"`
-	ImageURL    string `json:"image_url"`
-	Active      *bool  `json:"active"`
+	CategoryID  *string `json:"category_id"`
+	Name        string  `json:"name"`
+	Slug        string  `json:"slug"`
+	Description string  `json:"description"`
+	SKU         string  `json:"sku"`
+	Price       *int64  `json:"price"`
+	Currency    string  `json:"currency"`
+	ImageURL    string  `json:"image_url"`
+	Active      *bool   `json:"active"`
 }
 
 type ProductListResponse struct {
